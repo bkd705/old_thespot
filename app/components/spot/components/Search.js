a621518df8b5d6c176858d35e7deca88c51a6fdf
bkd705 <comparison@live.com>
2016-12-07 22:16:34 -0500

    Added clear button to search

--- app/components/spot/components/Search.js
+++ app/components/spot/components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import { tags } from '../../../data/data'
 
@@ -6,11 +7,16 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      hideDelete: true
+    }
+
     this.addTagFilter = this.addTagFilter.bind(this)
   }
-  
+
   doSearch() {
-    let query = this.refs.searchInput.value;
+    this.refs.searchInput.value.length === 0 ? this.setState({ hideDelete: true }) : this.setState({ hideDelete: false })
+    const query = this.refs.searchInput.value;
     this.props.doSearch(query);
   }
 
@@ -19,13 +25,24 @@ class Search extends React.Component {
     this.props.doSearch(tag);
   }
 
+  _emptyField() {
+    this.refs.searchInput.value = ''
+    this.doSearch()
+    this.setState({
+      hideDelete: true
+    })
+  }
+
   render(){
     const taglist = tags.map((tag, key) => {
       return <li key={key} onClick={() => this.addTagFilter(tag)}>+{tag}</li>
     })
+
+    const { hideDelete } = this.state
     return(
-      <div className="search">
-        <input type="text" className="search__input" ref="searchInput" name='filter' onChange={this.doSearch.bind(this)} placeholder="Search Spots..." />
+      <div style={{position: 'relative'}} className='search'>
+        <input type="text" className="search__input" ref="searchInput" name='filter' onChange={this.doSearch.bind(this)} placeholder="Search by Feature or Name" />
+        <span className={classnames('search-delete', { 'delete-hidden': hideDelete})} onClick={this._emptyField.bind(this)}>&times;</span>
         <section className="search__filter">
           <h5>Popular Tags:</h5>
           <ul>
